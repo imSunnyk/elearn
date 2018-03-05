04754c901545fbd3493620d48ad0f3538795c420
Alex Pop <Appop99@gmail.com>
2018-03-05 14:26:30 +0200

    Added webroot

--- elearnapp/settings.py
+++ elearnapp/settings.py
@@ -15,6 +15,7 @@ from django.conf import global_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+WEBROOT_DIR = os.path.join( BASE_DIR, "../staticfiles" )
 
 
 TEMPLATE_DIRS = (
@@ -146,11 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/django-projects/elearnapp/static/',
-]
+STATIC_ROOT = os.path.join( WEBROOT_DIR, "static/" )
+STATICFILES_DIRS = (os.path.join(os.path.dirname(os.path.dirname(__file__)), "static"), )
 # Media files
 # https://docs.djangoproject.com/en/1.11/ref/settings/#std:setting-MEDIA_ROOT
 
